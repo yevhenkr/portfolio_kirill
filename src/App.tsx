a3ede047dee4b6ae1fa52f385f2a7a3ea8df8b0e
Eugen <vasilenkoevgen88@gmail.com>
2024-08-29 18:17:52 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+export function App() {
+  return (
+    <div>
+      <video controls>
+        <source src="../src/assets/video/fox.mp4" type="video/mp4" />
+        Ваш браузер не поддерживает воспроизведение видео.
+      </video>
+    </div>
+  )
+}
