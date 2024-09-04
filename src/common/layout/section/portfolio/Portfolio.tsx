a382bfae850bb69a8cb7f3a70ad0c469a5ae0bd5
Eugen <vasilenkoevgen88@gmail.com>
2024-09-04 16:06:28 +0300

    feat: add structure

--- src/common/layout/section/portfolio/Portfolio.tsx
+++ src/common/layout/section/portfolio/Portfolio.tsx
@@ -10,18 +10,33 @@ import {PortfolioCard} from "./PortfolioCard";
 export const Portfolio = () => {
     return (
         <StyledSection id={'Portfolio'}>
+            <TitleText>portfolio</TitleText>
             {/*<FlexWrapperWithMediaQuery $wrap={"wrap"} $gap={"20px"} $flex_direction={"row"} $justify={"space-around"}>*/}
             {/*    <PortfolioCard link={"https://vasilenko-evgen-xd8s.vercel.app/"} label={"Portfolio"} bgColor={"rgba(108,99,255,0.27)"} color={"#6C63FF"} img={first}/>*/}
             {/*    <PortfolioCard link={"https://github.com/MrEvgeniy1989/cards"} label={"Cards"} bgColor={"rgba(77,194,241,0.25)"} color={"#4581F6"} img={second}/>*/}
             {/*    <PortfolioCard link={"https://simpel-food.vercel.app/"} label={"Simple food"} bgColor={"rgba(0,0,0,0.22)"} color={`${myTheme.color.black}`} img={therd}/>*/}
             {/*</FlexWrapperWithMediaQuery>*/}
-            <PortfolioCard />
-            <PortfolioCard />
-            <PortfolioCard />
+            <PortfolioWrap>
+                <PortfolioCard/>
+                <PortfolioCard/>
+                <PortfolioCard/>
+            </PortfolioWrap>
         </StyledSection>
     );
 };
 
+const PortfolioWrap = styled.div`
+    display: flex;
+`;
+
+
+const TitleText = styled.h2`
+    font-size: 120px;
+    color: #fff;
+    position: relative;
+    z-index: 1;
+    text-align: center;
+`;
 
 const StyledSection = styled.section`
     padding-bottom: 170px;
