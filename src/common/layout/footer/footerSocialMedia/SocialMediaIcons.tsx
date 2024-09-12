fedf9de61e6d116734d40ebf500e61dd68883721
Eugen <vasilenkoevgen88@gmail.com>
2024-09-12 11:42:42 +0300

    feat: change icons

--- src/common/layout/footer/footerSocialMedia/SocialMediaIcons.tsx
+++ src/common/layout/footer/footerSocialMedia/SocialMediaIcons.tsx
@@ -4,38 +4,41 @@ import {FacebookIcon} from "../../../../assets/icons/facebookIcon.tsx";
 import {InstagramIcon} from "../../../../assets/icons/instagramIcon.tsx";
 import {LinkedinIcon} from "../../../../assets/icons/linkedinIcon.tsx";
 import {TelegramIcon} from "../../../../assets/icons/telegramIcon.tsx";
+import {VimeoIcon} from "../../../../assets/icons/vimeoIcon.tsx";
 
 const SocialMediaIcons = () => {
-  return (
-    <SocialList>
-      <SocialItem>
-        <SocialLink href={'https://www.facebook.com/share/CEyuLRthp5tg6zPc/'} rel={'noreferrer'}>
-            <FacebookIcon fill={`${myTheme.color.grey}`}/>
-        </SocialLink>
-      </SocialItem>
+    return (
+        <SocialList>
+            <SocialItem>
+                <SocialLink
+                    href={'https://www.linkedin.com/in/kirill-yakubov-1792972a8?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app'}
+                    rel={'noreferrer'}
+                >
+                    <LinkedinIcon fill={`${myTheme.color.grey}`}/>
+                </SocialLink>
+            </SocialItem>
+            <SocialItem>
+                <SocialLink href={'https://www.facebook.com/share/CEyuLRthp5tg6zPc/'} rel={'noreferrer'}>
+                    <FacebookIcon fill={`${myTheme.color.grey}`}/>
+                </SocialLink>
+            </SocialItem>
 
-      <SocialItem>
-        <SocialLink
-          href={'https://www.linkedin.com/in/kirill-yakubov-1792972a8?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app'}
-          rel={'noreferrer'}
-        >
-            <LinkedinIcon fill={`${myTheme.color.grey}`}/>
-        </SocialLink>
-      </SocialItem>
+            <SocialItem>
+                <SocialLink href={'https://www.instagram.com/kirill.yakubov?igsh=MTFxeno0MXFnM2t5Nw=='}
+                            rel={'noreferrer'}>
+                    <InstagramIcon fill={`${myTheme.color.grey}`}/>
+                </SocialLink>
+            </SocialItem>
 
-      <SocialItem>
-        <SocialLink href={'https://t.me/Kiwi_Media'} rel={'noreferrer'}>
-            <TelegramIcon fill={`${myTheme.color.grey}`}/>
-        </SocialLink>
-      </SocialItem>
 
-        <SocialItem>
-        <SocialLink href={'https://www.instagram.com/kirill.yakubov?igsh=MTFxeno0MXFnM2t5Nw=='} rel={'noreferrer'}>
-            <InstagramIcon fill={`${myTheme.color.grey}`}/>
-        </SocialLink>
-      </SocialItem>
-    </SocialList>
-  )
+            <SocialItem>
+                <SocialLink href={'https://t.me/Kiwi_Media'} rel={'noreferrer'}>
+                    <VimeoIcon fill={`${myTheme.color.grey}`}/>
+                </SocialLink>
+            </SocialItem>
+
+        </SocialList>
+    )
 }
 
 export default SocialMediaIcons
@@ -58,7 +61,7 @@ const SocialItem = styled.li`
     align-items: center; /* Центрирует содержимое по вертикали */
 `
 const SocialLink = styled.a`
-   display: flex;
+    display: flex;
     justify-content: center;
     align-items: center;
     text-decoration: none;
